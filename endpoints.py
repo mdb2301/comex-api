9a51189bdc4c4b794654739a433005992077130f
unknown <dashcroc2601@gmail.com>
2021-01-30 14:38:17 +0530

    v2

--- endpoints.py
+++ endpoints.py
@@ -95,7 +95,7 @@ class AddBook(Resource):
                 "name":data["name"],
                 "authors":data["authors"],
                 "pages":data["pages"],
-                "uploaded_on":datetime.now().strftime("%Y/%m/%d"),
+                "uploaded_on":datetime.now().strftime("%d/%m/%Y"),
                 "description":data["description"],
                 "avg_rating":data["avg_rating"],
                 "thumb_link":data["thumb_link"],
